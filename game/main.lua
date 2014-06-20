1d07c7c167da9503d7ceba47b9f485fff37ab33d
Paul Bienkowski <opatutlol@aol.com>
2014-06-20 22:56:36 +0200

    Some basics

--- game/main.lua
+++ game/main.lua
@@ -1,4 +1,10 @@
+class = require("utils/class") -- middleclass
+require("utils/vector")
+require("utils/helper")
+require("utils/resources")
 
+
+-- love hooks
 
 function love.init()
 end
@@ -8,4 +14,10 @@ end
 
 function love.draw()
     love.graphics.print("Hello World", 10, 10)
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
